b28fc7d1dd6e8c0dcb97bf82a81fb2a0e8758f96
nishanth <mksabarinishanth@gmail.com>
2025-08-12 21:08:53 +0530

    Home Scree Finished

--- src/Medi-Bridge-App/Main-Full-Screen/User-View-Screen/Content-screen/Home-app/Home-app.tsx
+++ src/Medi-Bridge-App/Main-Full-Screen/User-View-Screen/Content-screen/Home-app/Home-app.tsx
@@ -3,10 +3,84 @@ import { AddPatientIcon } from '../../../../../assets/icons/SVGCODE';
 import Appp, {
   PatientDisplayScreen,
 } from '../Patient-History/Patient-Display-Screen/PatientDisplayScreen';
+import { HomeScreenCard } from './HomeScreenCard/HomeScreenCard';
+import './Home-app.css';
+import { ROUTES } from '../../../../types/DATATYPES';
+import { useNavigate } from 'react-router-dom';
+
 export function HomeScreen() {
+  const navigate = useNavigate();
+
+  const first = {
+    headerText: 'Book Appointment and Patients',
+    text1: 'Book',
+    text2: 'Revisit',
+    bottomText: 'Saved',
+    gradient: {
+      background: 'linear-gradient(to bottom right, #f8d22e, #ffb969)',
+    },
+    onclick: () => navigate(ROUTES.ADDPATIENT),
+  };
+  const second = {
+    headerText: 'Pending Appointment',
+    text1: 'Swap',
+    text2: 'Deadline',
+    bottomText: 'Saved',
+    gradient: {
+      background: 'linear-gradient(to bottom right, #e948a9, #f8488e)',
+    },
+    onclick: () => navigate(ROUTES.PENDINGAPPOINTMENT),
+  };
+  const third = {
+    headerText: 'Finished Appointment & Add History',
+    text1: 'Swap',
+    text2: 'Deadline',
+    bottomText: 'Saved',
+    gradient: {
+      background: 'linear-gradient(135deg, #a855f7, #8b5cf6)',
+    },
+    onclick: () => navigate(ROUTES.FINISHEDAPPOINTMENT),
+  };
+  const fourth = {
+    headerText: 'Patient History & other details',
+    text1: 'Swap',
+    text2: 'Deadline',
+    bottomText: 'Saved',
+    gradient: {
+      background: 'linear-gradient(135deg, #60a5fa, #3b82f6)',
+    },
+    onclick: () => navigate(ROUTES.PATIENTHISTORY),
+  };
+  const fifth = {
+    headerText: 'Revenue Analysis & Balance Sheet',
+    text1: 'Swap',
+    text2: 'Deadline',
+    bottomText: 'Saved',
+    gradient: {
+      background: 'linear-gradient(135deg, #a855f7, #8b5cf6)',
+    },
+    onclick: () => navigate(ROUTES.REVENUEANALYSIS),
+  };
+  const sixth = {
+    headerText: 'Doctor Performance',
+    text1: 'Book',
+    text2: 'Revisit',
+    bottomText: 'Saved',
+    gradient: {
+      background: 'linear-gradient(to bottom right, #f8d22e, #ffb969)',
+    },
+    onclick: () => navigate(ROUTES.DOCTORPERFORMANCEANALYSIS),
+  };
   return (
     <>
-      <h1>HOme Screedn</h1>
+      <div className="homeScreendsfa">
+        <HomeScreenCard {...first}></HomeScreenCard>
+        <HomeScreenCard {...second}></HomeScreenCard>
+        <HomeScreenCard {...third}></HomeScreenCard>
+        <HomeScreenCard {...fourth}></HomeScreenCard>
+        <HomeScreenCard {...fifth}></HomeScreenCard>
+        <HomeScreenCard {...sixth}></HomeScreenCard>
+      </div>
     </>
   );
 }
---
+++ src/Medi-Bridge-App/Main-Full-Screen/User-View-Screen/Content-screen/Home-app/HomeScreenCard/HomeScreenCard.tsx
@@ -0,0 +1,34 @@
+import dseth from '../../../../../../assets/Logo/3dseth.avif';
+import { IoIosStar } from 'react-icons/io';
+import { MdOutlineHealthAndSafety } from 'react-icons/md';
+import './HomeScreenCard.css';
+export function HomeScreenCard({ headerText, text1, text2, bottomText, onclick, gradient }) {
+  return (
+    <>
+      <div className="homeScreenCard" onClick={onclick} style={gradient}>
+        <div className="starter-text">
+          <p className="starter-actual-text">24/7 Hospital</p>
+          <MdOutlineHealthAndSafety size={24}></MdOutlineHealthAndSafety>
+        </div>
+        <div className="header-text-home-scree-app">
+          <div className="midsssss">
+            <h1 className="header-acutal-text-hs">
+              {headerText ?? 'Book Appointment and Patients'}
+            </h1>
+            <div className="some-dummy-texting">
+              <p>{text1 ?? 'text-1'}</p>
+              <p>{text2 ?? 'text-2'}</p>
+            </div>
+          </div>
+          <div className="home-screen-card-header-img-container">
+            <div className="someanotherdummytext">
+              <IoIosStar size={24}></IoIosStar>
+              <p>Saved</p>
+            </div>
+            <img src={dseth} alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
