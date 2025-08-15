d8dbb883921b84aed5736ba3b23d47c217ccadd7
nishanth <mksabarinishanth@gmail.com>
2025-08-15 16:09:45 +0530

    css import some

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-
+import './Medi-Bridge-App/Main-Full-Screen/User-View-Screen/Content-screen/Pending-Appointments/Pending-Form/pending-form.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
