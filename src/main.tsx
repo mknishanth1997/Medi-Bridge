563b21998d8d1053e28518caf7da0636e60e7ccc
nishanth <mksabarinishanth@gmail.com>
2025-08-15 16:05:38 +0530

    css import trying for solution 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import.meta.glob(['./**/*.css'], { eager: true });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
