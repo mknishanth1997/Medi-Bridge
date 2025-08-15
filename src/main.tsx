e5340fd9f196b6831f1bb78f717f5c537235fc08
nishanth <mksabarinishanth@gmail.com>
2025-08-15 16:07:08 +0530

    reoved the previous change

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import.meta.glob(['./**/*.css'], { eager: true });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
