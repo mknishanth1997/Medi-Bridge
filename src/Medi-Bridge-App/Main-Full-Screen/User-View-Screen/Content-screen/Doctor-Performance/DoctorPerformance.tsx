093b014effb6e3b37c93fa4105651dc9f9e48735
nishanth <mksabarinishanth@gmail.com>
2025-08-15 16:44:25 +0530

    remvoed the prvious change another time

--- src/Medi-Bridge-App/Main-Full-Screen/User-View-Screen/Content-screen/Doctor-Performance/DoctorPerformance.tsx
+++ src/Medi-Bridge-App/Main-Full-Screen/User-View-Screen/Content-screen/Doctor-Performance/DoctorPerformance.tsx
@@ -2,6 +2,7 @@ import './DoctorPerformance.css';
 import { TestHeader } from '../../../../../Testing/TestComponent-1/TestComp1';
 import AgeGroupRevenueChart from '../Revenue-Analysis/Revenue-components/Pie1/RenderCustomizedTable';
 import { AddPatientDetailsForm } from '../Add-New-Patient/AppointmentForm/AddAppointmentForm';
+import './../Add-New-Patient/AppointmentForm/AddAppointmentForm.css';
 
 export function DoctorPerformanceAnalysis() {
   return <AddPatientDetailsForm></AddPatientDetailsForm>;
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from './App.tsx';
 import './Medi-Bridge-App/Main-Full-Screen/User-View-Screen/Content-screen/Pending-Appointments/Pending-Form/pending-form.css';
 // main.tsx
 // Force import all CSS files inside src (recursively)
-import.meta.glob('./**/*.css', { eager: true });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
