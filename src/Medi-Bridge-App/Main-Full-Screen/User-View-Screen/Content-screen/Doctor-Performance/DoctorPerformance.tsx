8a90766fff64548ae098d7ff270fdad6cb4f2151
nishanth <mksabarinishanth@gmail.com>
2025-08-15 16:31:09 +0530

    testing

--- src/Medi-Bridge-App/Main-Full-Screen/User-View-Screen/Content-screen/Doctor-Performance/DoctorPerformance.tsx
+++ src/Medi-Bridge-App/Main-Full-Screen/User-View-Screen/Content-screen/Doctor-Performance/DoctorPerformance.tsx
@@ -1,7 +1,8 @@
 import './DoctorPerformance.css';
 import { TestHeader } from '../../../../../Testing/TestComponent-1/TestComp1';
 import AgeGroupRevenueChart from '../Revenue-Analysis/Revenue-components/Pie1/RenderCustomizedTable';
+import { AddPatientDetailsForm } from '../Add-New-Patient/AppointmentForm/AddAppointmentForm';
 
 export function DoctorPerformanceAnalysis() {
-  return <AgeGroupRevenueChart></AgeGroupRevenueChart>;
+  return <AddPatientDetailsForm></AddPatientDetailsForm>;
 }
